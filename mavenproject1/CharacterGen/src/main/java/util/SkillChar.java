4022555d994604862b0c6b8f672b4a4a66439f10
dezilius <dezilius@dezilius-pc>
2020-02-25 18:02:09 +0100

    Added deafault skill set function

--- mavenproject1/CharacterGen/src/main/java/util/SkillChar.java
+++ mavenproject1/CharacterGen/src/main/java/util/SkillChar.java
@@ -14,8 +14,67 @@ import java.util.ArrayList;
 public class SkillChar {
     
     public static void setDefaultValues(ArrayList<javax.swing.JTextField> skillFields) {
-        for (javax.swing.JTextField iter : skillFields) {
-            iter.setText("0");
-        }
+
+            skillFields.get(0).setText("5");
+            skillFields.get(1).setText("1");
+            skillFields.get(2).setText("5");
+            skillFields.get(3).setText("1");
+            skillFields.get(4).setText("5");
+            skillFields.get(5).setText("0");
+            skillFields.get(6).setText("0");
+            skillFields.get(7).setText("15");
+            skillFields.get(8).setText("20");
+            skillFields.get(9).setText("0");
+            skillFields.get(10).setText("0");
+            skillFields.get(11).setText("5");
+            skillFields.get(12).setText("11");
+            skillFields.get(13).setText("20");
+            skillFields.get(14).setText("10");
+            skillFields.get(15).setText("5");
+            skillFields.get(16).setText("25");
+            skillFields.get(17).setText("0");
+            skillFields.get(18).setText("0");
+            skillFields.get(19).setText("20");
+            skillFields.get(20).setText("25");
+            skillFields.get(21).setText("0");
+            skillFields.get(22).setText("30");
+            skillFields.get(23).setText("5");
+            skillFields.get(24).setText("15");
+            skillFields.get(25).setText("20");
+            skillFields.get(26).setText("1");
+            skillFields.get(27).setText("0");
+            skillFields.get(28).setText("0");
+            skillFields.get(29).setText("11");
+            skillFields.get(30).setText("5");
+            skillFields.get(31).setText("20");
+            skillFields.get(32).setText("20");
+            skillFields.get(33).setText("1");
+            skillFields.get(34).setText("10");
+            skillFields.get(35).setText("1");
+            skillFields.get(36).setText("10");
+            skillFields.get(37).setText("10");
+            skillFields.get(38).setText("5");
+            skillFields.get(39).setText("1");
+            skillFields.get(40).setText("10");
+            skillFields.get(41).setText("1");
+            skillFields.get(42).setText("10");
+            skillFields.get(43).setText("1");
+            skillFields.get(44).setText("5");
+            skillFields.get(45).setText("1");
+            skillFields.get(46).setText("0");
+            skillFields.get(47).setText("0");
+            skillFields.get(48).setText("10");
+            skillFields.get(49).setText("25");
+            skillFields.get(50).setText("20");
+            skillFields.get(51).setText("10");
+            skillFields.get(52).setText("20");
+            skillFields.get(53).setText("20");
+            skillFields.get(54).setText("10");
+            skillFields.get(55).setText("10");
+            skillFields.get(56).setText("10");
+            skillFields.get(57).setText("10");
+            skillFields.get(58).setText("10");
+            skillFields.get(59).setText("10");
+            
     }
 }
